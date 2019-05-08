a3954ae58e24b14301e4533e7801e9ba26cd4eb5
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2019-05-08 14:23:44 -0700

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -3,27 +3,27 @@
 import time 
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT) # Connected to PWMA 
-GPIO.setup(11, GPIO.OUT) # Connected to AIN2 
-GPIO.setup(12, GPIO.OUT) # Connected to AIN1 
-GPIO.setup(13, GPIO.OUT) # Connected to STBY
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.OUT) # Connected to PWMA 
+GPIO.setup(17, GPIO.OUT) # Connected to AIN2 
+GPIO.setup(18, GPIO.OUT) # Connected to AIN1 
+GPIO.setup(27, GPIO.OUT) # Connected to STBY
 #drive motor clockwise
-GPIO.output(12, GPIO.HIGH) # Set AIN1 
-GPIO.output(11, GPIO.LOW) # Set AIN2
+GPIO.output(18, GPIO.HIGH) # Set AIN1 
+GPIO.output(17, GPIO.LOW) # Set AIN2
 #set motor speed
-GPIO.output(7, GPIO.HIGH) # Set PWMA
+GPIO.output(4, GPIO.HIGH) # Set PWMA
 #disable standby
-GPIO.output(13, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
 time.sleep(5) #run for 5 seconds
 #drive motor CCW
-GPIO.output(12, GPIO.LOW) # Set AIN1 
-GPIO.output(11, GPIO.HIGH) # Set AIN2
-GPIO.output(7, GPIO.HIGH) # Set PWMA
-GPIO.output(13, GPIO.HIGH)
+GPIO.output(18, GPIO.LOW) # Set AIN1 
+GPIO.output(17, GPIO.HIGH) # Set AIN2
+GPIO.output(4, GPIO.HIGH) # Set PWMA
+GPIO.output(27, GPIO.HIGH)
 time.sleep(5)
 #reset all gpio pins
-GPIO.output(12, GPIO.LOW) # Set AIN1 
-GPIO.output(11, GPIO.LOW) # Set AIN2 
-GPIO.output(7, GPIO.LOW) # Set PWMA 
-GPIO.output(13, GPIO.LOW) # Set STBY
+GPIO.output(18, GPIO.LOW) # Set AIN1 
+GPIO.output(17, GPIO.LOW) # Set AIN2 
+GPIO.output(4, GPIO.LOW) # Set PWMA 
+GPIO.output(27, GPIO.LOW) # Set STBY
