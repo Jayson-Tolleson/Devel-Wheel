b999f4dfd41ae8e8260249c3a53181fbb5f94d56
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2019-05-09 12:40:22 -0700

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -15,33 +15,33 @@ gpio.setcfg(connector.GPIOp16, 1)  #pin 16 as output bin2
 gpio.setcfg(connector.GPIOp18, 1)  #pin 18 as output pwmb
 
 #drive motor counterclockwise
-gpio﻿.output﻿(﻿connector﻿.GPIOp11, 0) #set gpio to low ain1
-gpio﻿.output﻿(﻿connector﻿.GPIOp12, 1) #set gpio to high ain2
-gpio﻿.output﻿(﻿connector﻿.GPIOp7, 1) #set gpio to high pwma
-gpio﻿.output﻿(﻿connector﻿.GPIOp13, 1) #set gpio to high stdby
+gpio.output(connector.GPIOp11, 0) #set gpio to low ain1
+gpio.output(connector.GPIOp12, 1) #set gpio to high ain2
+gpio.output(connector.GPIOp7, 1) #set gpio to high pwma
+gpio.output(connector.GPIOp13, 1) #set gpio to high stdby
 time.sleep(10)
 
 #reset all gpio pins
-gpio﻿.output﻿(﻿connector﻿.GPIOp11, 0) #reset pin11 ain1
-gpio﻿.output﻿(﻿connector﻿.GPIOp12, 0) #reset pin12 ain2
-gpio﻿.output﻿(﻿connector﻿.GPIOp7, 0) #reset pin7 pwma
-gpio﻿.output﻿(﻿connector﻿.GPIOp13, 0) #reset pin13 stdby
-gpio﻿.output﻿(﻿connector﻿.GPIOp15, 0) #reset pin15 bin1
-gpio﻿.output﻿(﻿connector﻿.GPIOp16, 0) #reset pin16 bin2
-gpio﻿.output﻿(﻿connector﻿.GPIOp18, 0) #reset pin18 pwmb
+gpio.output(connector.GPIOp11, 0) #reset pin11 ain1
+gpio.output(connector.GPIOp12, 0) #reset pin12 ain2
+gpio.output(connector.GPIOp7, 0) #reset pin7 pwma
+gpio.output(connector.GPIOp13, 0) #reset pin13 stdby
+gpio.output(connector.GPIOp15, 0) #reset pin15 bin1
+gpio.output(connector.GPIOp16, 0) #reset pin16 bin2
+gpio.output(connector.GPIOp18, 0) #reset pin18 pwmb
 
 #drive motor clockwise
-gpio﻿.output﻿(﻿connector﻿.GPIOp15, 1) #set gpio to low bin1
-gpio﻿.output﻿(﻿connector﻿.GPIOp16, 0) #set gpio to high bin2
-gpio﻿.output﻿(﻿connector﻿.GPIOp18, 1) #set gpio to high pwmb
-gpio﻿.output﻿(﻿connector﻿.GPIOp13, 1) #set gpio to high stdby
+gpio.output(connector.GPIOp15, 1) #set gpio to low bin1
+gpio.output(connector.GPIOp16, 0) #set gpio to high bin2
+gpio.output(connector.GPIOp18, 1) #set gpio to high pwmb
+gpio.output(connector.GPIOp13, 1) #set gpio to high stdby
 time.sleep(15)
 
 #reset all gpio pins
-gpio﻿.output﻿(﻿connector﻿.GPIOp11, 0) #reset pin11 ain1
-gpio﻿.output﻿(﻿connector﻿.GPIOp12, 0) #reset pin12 ain2
-gpio﻿.output﻿(﻿connector﻿.GPIOp7, 0) #reset pin7 pwma
-gpio﻿.output﻿(﻿connector﻿.GPIOp13, 0) #reset pin13 stdby
-gpio﻿.output﻿(﻿connector﻿.GPIOp15, 0) #reset pin15 bin1
-gpio﻿.output﻿(﻿connector﻿.GPIOp16, 0) #reset pin16 bin2
-gpio﻿.output﻿(﻿connector﻿.GPIOp18, 0) #reset pin18 pwmb
+gpio.output(connector.GPIOp11, 0) #reset pin11 ain1
+gpio.output(connector.GPIOp12, 0) #reset pin12 ain2
+gpio.output(connector.GPIOp7, 0) #reset pin7 pwma
+gpio.output(connector.GPIOp13, 0) #reset pin13 stdby
+gpio.output(connector.GPIOp15, 0) #reset pin15 bin1
+gpio.output(connector.GPIOp16, 0) #reset pin16 bin2
+gpio.output(connector.GPIOp18, 0) #reset pin18 pwmb
