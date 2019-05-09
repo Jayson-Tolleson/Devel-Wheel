16c25ed54eebfbbe9ce6556e59f80e784cf48959
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2019-05-09 15:55:09 -0700

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -1,47 +1,48 @@
 #!/usr/bin/env python3
 #import required modules
-import os, sys
-from pyGPIO.gpio import gpio, connector
 import time
+import pigpio
+#works on pi and le potato and asus tinkerboard
+# Connect to local Pi.
+pi = pigpio.pi() 
 
 #set gpio pins
-gpio.init()
-gpio.setcfg(connector.GPIOp7, 1)  #pin 7 as output pwma
-gpio.setcfg(connector.GPIOp11, 1)  #pin 11 as output ain1
-gpio.setcfg(connector.GPIOp12, 1)  #pin 12 as output ain2
-gpio.setcfg(connector.GPIOp13, 1)  #pin 13 as output stdby
-gpio.setcfg(connector.GPIOp15, 1)  #pin 15 as output bin1
-gpio.setcfg(connector.GPIOp16, 1)  #pin 16 as output bin2
-gpio.setcfg(connector.GPIOp18, 1)  #pin 18 as output pwmb
+pi.set_mode(7, pigpio.OUTPUT)  #pin 7 as output pwma
+pi.set_mode(11, pigpio.OUTPUT) #pin 11 as output ain1
+pi.set_mode(12, pigpio.OUTPUT) #pin 12 as output ain2
+pi.set_mode(13, pigpio.OUTPUT) #pin 13 as output stdby
+pi.set_mode(15, pigpio.OUTPUT) #pin 15 as output bin1
+pi.set_mode(16, pigpio.OUTPUT) #pin 16 as output bin2
+pi.set_mode(18, pigpio.OUTPUT) #pin 18 as output pwmb
 
 #drive motor counterclockwise
-gpio.output(connector.GPIOp11, 0) #set gpio to low ain1
-gpio.output(connector.GPIOp12, 1) #set gpio to high ain2
-gpio.output(connector.GPIOp7, 1) #set gpio to high pwma
-gpio.output(connector.GPIOp13, 1) #set gpio to high stdby
-time.sleep(10)
+pi.write(11,0) #set gpio11 to low ain1
+pi.write(12,1) #set gpio12 to low ain2
+pi.write(7,1)  #set gpio7 to high pwma
+pi.write(13,1) #set gpio13 to high stby
+time.sleep(10) #run for 10,000 ms
 
 #reset all gpio pins
-gpio.output(connector.GPIOp11, 0) #reset pin11 ain1
-gpio.output(connector.GPIOp12, 0) #reset pin12 ain2
-gpio.output(connector.GPIOp7, 0) #reset pin7 pwma
-gpio.output(connector.GPIOp13, 0) #reset pin13 stdby
-gpio.output(connector.GPIOp15, 0) #reset pin15 bin1
-gpio.output(connector.GPIOp16, 0) #reset pin16 bin2
-gpio.output(connector.GPIOp18, 0) #reset pin18 pwmb
+pi.write(11, 0) #reset pin11 ain1
+pi.write(12, 0) #reset pin12 ain2
+pi.write(7, 0) #reset pin7 pwma
+pi.write(13, 0) #reset pin13 stdby
+pi.write(15, 0) #reset pin15 bin1
+pi.write(16, 0) #reset pin16 bin2
+pi.write(18, 0) #reset pin18 pwmb
 
 #drive motor clockwise
-gpio.output(connector.GPIOp15, 1) #set gpio to low bin1
-gpio.output(connector.GPIOp16, 0) #set gpio to high bin2
-gpio.output(connector.GPIOp18, 1) #set gpio to high pwmb
-gpio.output(connector.GPIOp13, 1) #set gpio to high stdby
-time.sleep(15)
+pi.write(15, 1) #set gpio to low bin1
+pi.write(16, 0) #set gpio to high bin2
+pi.write(18, 1) #set gpio to high pwmb
+pi.write(13, 1) #set gpio to high stdby
+time.sleep(15)  #run for 15,000 ms
 
 #reset all gpio pins
-gpio.output(connector.GPIOp11, 0) #reset pin11 ain1
-gpio.output(connector.GPIOp12, 0) #reset pin12 ain2
-gpio.output(connector.GPIOp7, 0) #reset pin7 pwma
-gpio.output(connector.GPIOp13, 0) #reset pin13 stdby
-gpio.output(connector.GPIOp15, 0) #reset pin15 bin1
-gpio.output(connector.GPIOp16, 0) #reset pin16 bin2
-gpio.output(connector.GPIOp18, 0) #reset pin18 pwmb
+pi.write(11, 0) #reset pin11 ain1
+pi.write(12, 0) #reset pin12 ain2
+pi.write(7, 0) #reset pin7 pwma
+pi.write(13, 0) #reset pin13 stdby
+pi.write(15, 0) #reset pin15 bin1
+pi.write(16, 0) #reset pin16 bin2
+pi.write(18, 0) #reset pin18 pwmb
