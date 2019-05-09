edf2cb0572df2a2a02d2aad24cf6d025eaf2ccaf
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2019-05-09 12:38:09 -0700

    Update motortest.py

--- motortest.py
+++ motortest.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python3
-#working script for a two-wire 12v motor on a tb6612 motor driver with a raspberry pi, forward and reverse by switching the poles
-import time 
-import RPi.GPIO as GPIO
+#import required modules
+import os, sys
+from pyGPIO.gpio import gpio, connector
+import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT) # Connected to PWMA 
-GPIO.setup(17, GPIO.OUT) # Connected to AIN2 
-GPIO.setup(18, GPIO.OUT) # Connected to AIN1 
-GPIO.setup(27, GPIO.OUT) # Connected to STBY
+#set gpio pins
+gpio.init()
+gpio.setcfg(connector.GPIOp7, 1)  #pin 7 as output pwma
+gpio.setcfg(connector.GPIOp11, 1)  #pin 11 as output ain1
+gpio.setcfg(connector.GPIOp12, 1)  #pin 12 as output ain2
+gpio.setcfg(connector.GPIOp13, 1)  #pin 13 as output stdby
+gpio.setcfg(connector.GPIOp15, 1)  #pin 15 as output bin1
+gpio.setcfg(connector.GPIOp16, 1)  #pin 16 as output bin2
+gpio.setcfg(connector.GPIOp18, 1)  #pin 18 as output pwmb
+
+#drive motor counterclockwise
+gpio﻿.output﻿(﻿connector﻿.GPIOp11, 0) #set gpio to low ain1
+gpio﻿.output﻿(﻿connector﻿.GPIOp12, 1) #set gpio to high ain2
+gpio﻿.output﻿(﻿connector﻿.GPIOp7, 1) #set gpio to high pwma
+gpio﻿.output﻿(﻿connector﻿.GPIOp13, 1) #set gpio to high stdby
+time.sleep(10)
+
+#reset all gpio pins
+gpio﻿.output﻿(﻿connector﻿.GPIOp11, 0) #reset pin11 ain1
+gpio﻿.output﻿(﻿connector﻿.GPIOp12, 0) #reset pin12 ain2
+gpio﻿.output﻿(﻿connector﻿.GPIOp7, 0) #reset pin7 pwma
+gpio﻿.output﻿(﻿connector﻿.GPIOp13, 0) #reset pin13 stdby
+gpio﻿.output﻿(﻿connector﻿.GPIOp15, 0) #reset pin15 bin1
+gpio﻿.output﻿(﻿connector﻿.GPIOp16, 0) #reset pin16 bin2
+gpio﻿.output﻿(﻿connector﻿.GPIOp18, 0) #reset pin18 pwmb
+
 #drive motor clockwise
-GPIO.output(18, GPIO.HIGH) # Set AIN1 
-GPIO.output(17, GPIO.LOW) # Set AIN2
-GPIO.output(27, GPIO.HIGH) #disable standby
-time.sleep(5) #run for 5 seconds
+gpio﻿.output﻿(﻿connector﻿.GPIOp15, 1) #set gpio to low bin1
+gpio﻿.output﻿(﻿connector﻿.GPIOp16, 0) #set gpio to high bin2
+gpio﻿.output﻿(﻿connector﻿.GPIOp18, 1) #set gpio to high pwmb
+gpio﻿.output﻿(﻿connector﻿.GPIOp13, 1) #set gpio to high stdby
+time.sleep(15)
 
 #reset all gpio pins
-GPIO.output(18, GPIO.LOW) # Set AIN1 
-GPIO.output(17, GPIO.LOW) # Set AIN2 
-GPIO.output(4, GPIO.LOW) # Set PWMA 
-GPIO.output(27, GPIO.LOW) # Set STBY
-
-#set pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT) # Connected to PWMA 
-GPIO.setup(17, GPIO.OUT) # Connected to AIN2 
-GPIO.setup(18, GPIO.OUT) # Connected to AIN1 
-GPIO.setup(27, GPIO.OUT) # Connected to STBY
-
-#drive motor CCW
-GPIO.output(18, GPIO.LOW) # Set AIN1 
-GPIO.output(17, GPIO.HIGH) # Set AIN2
-GPIO.output(4, GPIO.HIGH) # Set PWMA
-GPIO.output(27, GPIO.HIGH)
-time.sleep(5)
-#reset all gpio pins
-GPIO.output(18, GPIO.LOW) # Set AIN1 
-GPIO.output(17, GPIO.LOW) # Set AIN2 
-GPIO.output(4, GPIO.LOW) # Set PWMA 
-GPIO.output(27, GPIO.LOW) # Set STBY
+gpio﻿.output﻿(﻿connector﻿.GPIOp11, 0) #reset pin11 ain1
+gpio﻿.output﻿(﻿connector﻿.GPIOp12, 0) #reset pin12 ain2
+gpio﻿.output﻿(﻿connector﻿.GPIOp7, 0) #reset pin7 pwma
+gpio﻿.output﻿(﻿connector﻿.GPIOp13, 0) #reset pin13 stdby
+gpio﻿.output﻿(﻿connector﻿.GPIOp15, 0) #reset pin15 bin1
+gpio﻿.output﻿(﻿connector﻿.GPIOp16, 0) #reset pin16 bin2
+gpio﻿.output﻿(﻿connector﻿.GPIOp18, 0) #reset pin18 pwmb
